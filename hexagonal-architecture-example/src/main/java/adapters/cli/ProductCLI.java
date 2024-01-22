99ee4185bfc910c6abc3faeeb7357ccfa0f91a13
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-01-22 15:37:02 -0300

    feat: adaptador de cli

---
+++ hexagonal-architecture-example/src/main/java/adapters/cli/ProductCLI.java
@@ -0,0 +1,12 @@
+package adapters.cli;
+
+import application.Product;
+import application.ProductService;
+
+public class ProductCLI {
+
+    public static void run(ProductService service, String action, Product product) {
+//        String result = "";
+//        switch ()
+    }
+}
