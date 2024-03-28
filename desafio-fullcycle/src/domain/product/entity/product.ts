58e10ffd19c0fb03a65445b7b453f2d7702737ed
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2024-03-28 07:22:46 -0300

    feat: alterando notification sem validação de yup

--- desafio-fullcycle/src/domain/product/entity/product.ts
+++ desafio-fullcycle/src/domain/product/entity/product.ts
@@ -43,6 +43,23 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate() {
-    return ProductValidatorFactory.create().validate(this)
+    if (this._id.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Id is required"
+      });
+    }
+    if (this._name.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Name is required"
+      })
+    }
+    if (this._price < 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Price must be greater than zero"
+      })
+    }
   }
 }
