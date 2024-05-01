58148fbb934f3bf1a657ccb9d0fc9c8ef77b55fa
fredsonchaves07 <fredson.ribeiro@objective.com.br>
2024-05-01 09:19:23 -0300

    feat: repository tests

---
+++ desafio-fullcycle-monolito/src/modules/invoice/repository/invoice.repository.spec.ts
@@ -0,0 +1,128 @@
+import { Sequelize } from "sequelize-typescript";
+import { InvoiceModel } from "./invoice.model";
+import InvoiceItemModel from "./invoice.item.model";
+import Invoice from "../domain/invoice.entity";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Address from "../../@shared/domain/value-object/address";
+import InvoiceItems from "../domain/invoice-items.entity";
+import InvoiceRepository from "./invoice.repository";
+
+
+
+describe("InvoiceRepository test", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true }
+        });
+        await sequelize.addModels([InvoiceModel, InvoiceItemModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a invoice", async () => {
+        const invoice = new Invoice({
+            id: new Id("1"),
+            name: "Lucian",
+            document: "1234-5678",
+            address: new Address(
+              "Rua 123",
+              "99",
+              "Casa Verde",
+              "Criciúma",
+              "SC",
+              "88888-888",
+            ),
+            items: [
+              new InvoiceItems({
+                  name: "Product 1",
+                  price: 100
+              }),
+              new InvoiceItems({
+                  name: "Product 2",
+                  price: 200
+              })
+            ]
+          })
+        const repository = new InvoiceRepository();
+        await repository.add(invoice);
+
+        const resultFind = await InvoiceModel.findOne({
+            where: { id: "1" },
+            include: [InvoiceItemModel],
+        });
+        expect(resultFind).toBeDefined();
+        expect(resultFind.id).toBe(invoice.id.id);
+        expect(resultFind.name).toBe(invoice.name);
+        expect(resultFind.document).toBe(invoice.document);
+        expect(resultFind.street).toBe(invoice.address.street);
+        expect(resultFind.number).toBe(invoice.address.number);
+        expect(resultFind.complement).toBe(invoice.address.complement);
+        expect(resultFind.city).toBe(invoice.address.city);
+        expect(resultFind.state).toBe(invoice.address.state);
+        expect(resultFind.zipcode).toBe(invoice.address.zipCode);
+        expect(resultFind.items).toHaveLength(2);
+        expect(resultFind.items[0].id).toBe(invoice.items[0].id.id);
+        expect(resultFind.items[0].name).toBe(invoice.items[0].name);
+        expect(resultFind.items[0].price).toBe(invoice.items[0].price);
+        expect(resultFind.items[1].id).toBe(invoice.items[1].id.id);
+        expect(resultFind.items[1].name).toBe(invoice.items[1].name);
+        expect(resultFind.items[1].price).toBe(invoice.items[1].price);
+    });
+
+    it("should find a invoice", async () => {
+        const invoice = new Invoice({
+            id: new Id("1"),
+            name: "Lucian",
+            document: "1234-5678",
+            address: new Address(
+              "Rua 123",
+              "99",
+              "Casa Verde",
+              "Criciúma",
+              "SC",
+              "88888-888",
+            ),
+            items: [
+              new InvoiceItems({
+                  name: "Product 1",
+                  price: 100
+              }),
+              new InvoiceItems({
+                  name: "Product 2",
+                  price: 200
+              })
+            ]
+          })
+        const repository = new InvoiceRepository();
+        await repository.add(invoice);
+
+        const resultFind = await repository.find(invoice.id.id);
+
+        expect(resultFind).toBeDefined();
+        expect(resultFind.id.id).toBe(invoice.id.id);
+        expect(resultFind.name).toBe(invoice.name);
+        expect(resultFind.document).toBe(invoice.document);
+        expect(resultFind.address.street).toBe(invoice.address.street);
+        expect(resultFind.address.number).toBe(invoice.address.number);
+        expect(resultFind.address.complement).toBe(invoice.address.complement);
+        expect(resultFind.address.city).toBe(invoice.address.city);
+        expect(resultFind.address.state).toBe(invoice.address.state);
+        expect(resultFind.address.zipCode).toBe(invoice.address.zipCode);
+        expect(resultFind.items).toHaveLength(2);
+        expect(resultFind.items[0].id.id).toBe(invoice.items[0].id.id);
+        expect(resultFind.items[0].name).toBe(invoice.items[0].name);
+        expect(resultFind.items[0].price).toBe(invoice.items[0].price);
+        expect(resultFind.items[1].id.id).toBe(invoice.items[1].id.id);
+        expect(resultFind.items[1].name).toBe(invoice.items[1].name);
+        expect(resultFind.items[1].price).toBe(invoice.items[1].price);
+        expect(resultFind.total).toBe(300);
+    });
+});
